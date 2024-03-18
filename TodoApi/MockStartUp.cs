0140e8701ac0300f52b0ade11028a6fa60af6e7f
bartlomiejkaczmarczyk <bartlomiej.kaczmarczyk@beqom.com>
2024-03-18 15:59:47 +0100

    added enpoints

---
+++ TodoApi/MockStartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoApi
+{
+    public class MockStartUp
+    {
+        public static ToDoApp.ToDoInMemoryDb Initialize()
+        {
+            var db = new ToDoApp.ToDoInMemoryDb();
+            db.Add("initial item");
+
+            return db;
+        }
+    }
+}
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,57 +1,14 @@
-using Microsoft.EntityFrameworkCore;
+using TodoApi;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-var todoItems = app.MapGroup("/todoitems");
+var takeDb = MockStartUp.Initialize();
 
-todoItems.MapGet("/",GetAllTodos);
+app.MapGet("/todos", () => takeDb.GetAllItems());
 
-static async Task<IResult> GetAllTodos(TodoDb db)
-{
-    return TypedResults.Ok(await db.Todos.ToArrayAsync());
-}
+app.MapPost("/add", (string text) => takeDb.Add(text));
 
-todoItems.MapGet("/complete", async (TodoDb db) => await db.Todos.Where(t => t.IsComplete).ToListAsync());
-
-todoItems.MapGet("/{id}", async (int id, TodoDb db) =>
-    await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
-
-todoItems.MapPost("/", async (Todo todo, TodoDb db) =>
-{
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/todoitems/{todo.Id}", todo);
-});
-
-todoItems.MapPut("/{id}", async (int id, Todo inputTodo, TodoDb db) =>
-{
-    var todo = await db.Todos.FindAsync(id);
-    
-    if(todo is null) return Results.NotFound();
-
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-todoItems.MapDelete("/{id}", async (int id, TodoDb db) =>
-{
-    if (await db.Todos.FindAsync(id) is Todo todo)
-    {
-        db.Todos.Remove(todo);
-        await db.SaveChangesAsync();
-
-        return Results.NoContent() ;
-    }
-
-    return Results.NotFound();
-});
+app.MapPut("/change-status",(int id) => takeDb.ChangeStatus(id));
 
 app.Run();
