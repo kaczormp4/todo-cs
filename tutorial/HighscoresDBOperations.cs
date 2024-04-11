afc7035033825fc9fba47019a2798f1f142d6a99
bartlomiejkaczmarczyk <bartlomiej.kaczmarczyk@beqom.com>
2024-04-11 12:00:59 +0200

    highscores refactoring

---
+++ tutorial/HighscoresDBOperations.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Xml.Serialization;
+
+namespace tutorial
+{
+    static class HighscoresDBOperations
+    {
+        static public string CreateDatabase(string dbName)
+        {
+
+            Console.WriteLine("test");
+            if (File.Exists(dbName))
+            {
+                File.Delete(dbName);
+            }
+
+            SQLiteConnection.CreateFile(dbName);
+
+            string connectionString = $"Data Source={dbName};Version=3;";
+
+            return connectionString;
+        }
+
+        static public void CreateTable(SQLiteConnection connection )
+        {
+            string sql = "Create Table highscores (name varchar(20), score int)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, connection);
+            command.ExecuteNonQuery();
+        }
+
+        static public void InsertHighscores(SQLiteConnection connection, string name, int score) 
+        {
+            string query = $"Insert into highscores (name, score) values ('{name}', {score})";
+            var command = new SQLiteCommand(query, connection);
+            command.ExecuteNonQuery();
+        }
+
+        static public List<HighScore> SelectHihgscoresByName(SQLiteConnection connection, string name)
+        {
+            SQLiteCommand selectCommand = new SQLiteCommand($"select * from highscores where name = '{name}'", connection);
+            var reader = selectCommand.ExecuteReader();
+
+            var HighScores = new List<HighScore>();
+
+            while (reader.Read())
+            {
+                var nameColumnId = reader.GetOrdinal("name");
+                var scoreColumnId = reader.GetOrdinal("score");
+
+                var _name = reader.GetString(nameColumnId);
+                var score = reader.GetInt32(scoreColumnId);
+
+                string message = $"Name:{_name} , Score:{score}";
+
+                Console.WriteLine(message);
+                HighScores.Add(new HighScore(_name, score));
+
+            }
+
+            return HighScores;
+        }
+
+        static public void UpdateScoreByName(SQLiteConnection connection, string nameCriteria, int newScore)
+        {
+            var newQuery = $"update highscores set score = {newScore} where name = '{nameCriteria}'";
+
+            SQLiteCommand updateCommand = new SQLiteCommand(newQuery, connection);
+            updateCommand.ExecuteNonQuery();
+        }
+    }
+}
--- tutorial/Program.cs
+++ tutorial/Program.cs
@@ -1,73 +1,27 @@
 ﻿// this creates a zero-byte file
 using System.Data.SQLite;
+using tutorial;
 
 const string DB_NAME = "MyDatabase.sqlite";
 
-Console.WriteLine("test");
-if (File.Exists(DB_NAME))
-{
-    File.Delete(DB_NAME);
-}
-SQLiteConnection.CreateFile(DB_NAME);
 
-string connectionString = $"Data Source={DB_NAME};Version=3;";
+
+string connectionString = HighscoresDBOperations.CreateDatabase(DB_NAME);
+
 SQLiteConnection m_dbConnection = new SQLiteConnection(connectionString);
 m_dbConnection.Open();
 
-//varchar will likely be handled internally as TEXT
-// the (20) will be ignored
-// see https://www.sqlite.org/datatype3.html#affinity_name_examples
-string sql = "Create Table highscores (name varchar(20), score int)";
-//// you could also write sql = "CREATE TABLE IF NOT EXISTS highscores ..."
-SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
-command.ExecuteNonQuery();
+HighscoresDBOperations.CreateTable(m_dbConnection);
 
-sql = "Insert into highscores (name, score) values ('Me', 9001)";
-command = new SQLiteCommand(sql, m_dbConnection);
-command.ExecuteNonQuery();
+HighscoresDBOperations.InsertHighscores(m_dbConnection, "me", 9221);
 
-SQLiteCommand selectCommand = new SQLiteCommand("select * from highscores where name = 'Me' limit 1", m_dbConnection);
-var reader = selectCommand.ExecuteReader();
+var highScores = HighscoresDBOperations.SelectHihgscoresByName(m_dbConnection, "me");
 
-HighScore newRecord = null;
+highScores.ForEach(highScore => HighscoresDBOperations.UpdateScoreByName(m_dbConnection, highScore.name, 22222));
 
-while (reader.Read())
-{
-    var nameColumnId = reader.GetOrdinal("name");
-    var scoreColumnId = reader.GetOrdinal("score");
 
-    var name = reader.GetString(nameColumnId);
-    var score = reader.GetInt32(scoreColumnId);
+HighscoresDBOperations.SelectHihgscoresByName(m_dbConnection, "me");
 
-    string message = $"Name:{name} , Score:{score}";
-
-    Console.WriteLine(message);
-
-    newRecord = new HighScore(name, score).Add(10);
-
-}
-
-var newQuery = $"update highscores set score = {newRecord?.score} where name = '{newRecord?.name}'";
-
-SQLiteCommand updateCommand = new SQLiteCommand(newQuery, m_dbConnection);
-updateCommand.ExecuteNonQuery();
-
-SQLiteCommand selectCommand2 = new SQLiteCommand("select * from highscores where name = 'Me' limit 1", m_dbConnection);
-var reader2 = selectCommand2.ExecuteReader();
-
-while (reader2.Read())
-{
-    var nameColumnId = reader2.GetOrdinal("name");
-    var scoreColumnId = reader2.GetOrdinal("score");
-
-    var name = reader2.GetString(nameColumnId);
-    var score = reader2.GetInt32(scoreColumnId);
-
-    string message = $"Name:{name} , Score:{score}";
-
-    Console.WriteLine(message);
-
-}
 m_dbConnection.Close();
 
 class HighScore
