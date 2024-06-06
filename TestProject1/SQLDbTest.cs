4f427cda631257c946a867c5e7b26e10da7e3969
bartlomiejkaczmarczyk <bartlomiej.kaczmarczyk@beqom.com>
2024-06-06 11:34:44 +0200

    unit tests

--- TestProject1/SQLDbTest.cs
+++ TestProject1/SQLDbTest.cs
@@ -86,7 +86,67 @@ namespace TestProject1
 
             // Assert
             Assert.False(result);
-
         }
+
+        [Fact]
+        public void Should_get_item_by_id_and_return_correct_item()
+        {
+            // Arrange
+            var db = new ToDoApp.ToDoSQLiteDB();
+            db.Initialize(Guid.NewGuid().ToString(), true);
+
+            var value = "example1";
+            db.Add(value);
+            var items = db.GetAllItems();
+
+            var id = items.First(x => x.Value == value).Id;
+
+            // Act
+            var item = db.GetItemById(id);
+
+            // Assert
+            Assert.Equal(value, item.Value);
+        }
+
+        [Fact]
+        public void Should_not_get_item_by_id_and_when_item_doesnt_exist()
+        {
+            // Arrange
+            var db = new ToDoApp.ToDoSQLiteDB();
+            db.Initialize(Guid.NewGuid().ToString(), true);
+
+            var value = "example1";
+            db.Add(value);
+            var items = db.GetAllItems();
+
+            var notExistedId = items.Select(x => x.Id).Max() + 100;
+
+            // Act
+            var item = db.GetItemById(notExistedId);
+
+            // Assert
+            Assert.Null(item);
+        }
+
+        [Fact]
+        public void Should_get_all_items()
+        {
+            // Arrange
+            var db = new ToDoApp.ToDoSQLiteDB();
+            db.Initialize(Guid.NewGuid().ToString(), true);
+
+            // Act
+            var items1 = db.GetAllItems();
+            Assert.Empty(items1);
+
+            db.Add("example1");
+            db.Add("example2");
+            db.Add("example3");
+
+            // Assert
+            var items = db.GetAllItems();
+            Assert.Equal(3, items.Count());
+        }
+
     }
 }
